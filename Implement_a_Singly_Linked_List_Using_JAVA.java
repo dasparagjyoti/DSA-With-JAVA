74cd3094128c95e92328defebead6a38174f01ed
P.J.DAS@21 <paragjyotidas21@gmail.com>
2022-03-17 22:33:56 +0530

    Update Implement_a_Singly_Linked_List_Using_JAVA.java

--- Implement_a_Singly_Linked_List_Using_JAVA.java
+++ Implement_a_Singly_Linked_List_Using_JAVA.java
@@ -1,7 +1,7 @@
 package com.dsalgowithjava;
 
 public class Implement_a_Singly_Linked_List_Using_JAVA {
-    private ListNode head;
+    private ListNode head; //Instance Variable
 
     private static class  ListNode{
         private int data; //Data can be any Generic type,here we have taken int type only
