ca8645c30acd8915a1c665bf62c3a09c7f94650e
P.J.DAS@21 <paragjyotidas21@gmail.com>
2022-03-17 22:21:41 +0530

    Implement_a_Singly_Linked_List_Using_JAVA

---
+++ Implement_a_Singly_Linked_List_Using_JAVA.java
@@ -0,0 +1,16 @@
+package com.dsalgowithjava;
+
+public class Implement_a_Singly_Linked_List_Using_JAVA {
+    private ListNode head;
+
+    private static class  ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+}
