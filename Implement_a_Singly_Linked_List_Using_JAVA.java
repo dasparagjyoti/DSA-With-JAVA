c5deba0180fb3ac9a25dd516c1c4a2cbb6a57b0b
P.J.DAS@21 <paragjyotidas21@gmail.com>
2022-03-17 22:28:11 +0530

    Update Implement_a_Singly_Linked_List_Using_JAVA.java

--- Implement_a_Singly_Linked_List_Using_JAVA.java
+++ Implement_a_Singly_Linked_List_Using_JAVA.java
@@ -4,10 +4,10 @@ public class Implement_a_Singly_Linked_List_Using_JAVA {
     private ListNode head;
 
     private static class  ListNode{
-        private int data;
+        private int data; //Data can be any Generic type,here we have taken int type only
         private ListNode next;
 
-        public ListNode(int data){
+        public ListNode(int data){ //Constructor which contains the data
             this.data = data;
             this.next = null;
         }
